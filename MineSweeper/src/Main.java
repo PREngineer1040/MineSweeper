2619ea983e8a0db304d5de9e4aa1b72d4c5f214e
GrandDad7 <jose.tua@upr.edu>
2018-03-20 21:30:24 -0400

    Creating branch, making comment for possible code implementation

--- MineSweeper/src/Main.java
+++ MineSweeper/src/Main.java
@@ -2,6 +2,9 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		//boolean isGameOver = false;
+		
 		JFrame myFrame = new JFrame("Bienve's Grid");
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //This had to be changed, because otherwise the program would keep running after exiting the window. 
 		myFrame.setLocation(400, 150);
@@ -14,6 +17,17 @@ public class Main {
 		myFrame.addMouseListener(myMouseAdapter);
 		myFrame.setVisible(true);
 
-		
+//		if (isGameOver == true)
+//		{
+//			myFrame.setVisible(false);
+//			/*
+//			 * Trying to figure out a while or a possible if.
+//			 * If the game detects the gameover, boolean is switched to true and the window for the main minesweeper game is made invisible.
+//			 * The user is prompted with two selections from a dialoug box. 
+//			 * Choosing no causes the entire program to terminate and all windows to close.
+//			 * Choosing yes restarts the game from scratch and closes the dialogue box while making the primary window visible again.
+//			 */
+//			
+//		}
 	}
 }
